a5ab86aa0a18c8ebd9fbf3757745734398fac162
Mariam Olaya <mariamolaya1924@gmail.com>
2026-09-11 18:52:03 -0500

    Actualizacion automatica

--- programa.py
+++ programa.py
@@ -1,1 +1,10 @@
 import geopandas as gpd
+
+#Un geodataframe es una tabla de datos donde al menos una de sus columnas 
+# contienen datos espaciales vectoriales (geometricos) puntos, lineas
+# y poligonos
+
+# Leer archivos que tengan datos espaciales
+# Por ejemplo: Geojson
+
+archivo = gpd.read_file("custom.geo.json")
